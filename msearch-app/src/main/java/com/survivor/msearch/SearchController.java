ff70a19727f67cb1dd38db5fc7c59b16a0987dd2
survivor69 <survivor69@mLab>
2019-05-10 13:18:03 +0600

    Static Resource added (HTML View)

--- msearch-app/src/main/java/com/survivor/msearch/SearchController.java
+++ msearch-app/src/main/java/com/survivor/msearch/SearchController.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.survivor.msearch.config.SearchQueryBuilder;
@@ -18,8 +18,8 @@ public class SearchController {
 	@Autowired
 	private SearchQueryBuilder searchQueryBuilder;
 
-	@GetMapping(value = "/{text}")
-	public List<Job> getAll(@PathVariable final String text) {
+	@GetMapping(value = "/from/job")
+	public List<Job> getAll(@RequestParam("text") final String text) {
 		return searchQueryBuilder.getAll(text);
 	}
 }
