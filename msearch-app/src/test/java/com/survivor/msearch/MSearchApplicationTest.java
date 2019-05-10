a4649f5ed149f00a3e9bca1954a899367df298cc
survivor69 <survivor69@mLab>
2019-05-10 11:03:24 +0600

    TestClass Renamed

--- msearch-app/src/test/java/com/survivor/msearch/MSearchApplicationTest.java
+++ msearch-app/src/test/java/com/survivor/msearch/MSearchApplicationTest.java
@@ -1,4 +1,4 @@
-package com.techprimers.elastic;
+package com.survivor.msearch;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringElasticDemoApplicationTests {
+public class MSearchApplicationTest {
 
 	@Test
 	public void contextLoads() {
