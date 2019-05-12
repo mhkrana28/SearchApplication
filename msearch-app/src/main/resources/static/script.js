71ff9ad1cf1ec6345b014fd78ec8fc1633e19406
survivor69 <survivor69@mLab>
2019-05-12 22:51:10 +0600

    'Enter' key press is detected on search

--- msearch-app/src/main/resources/static/script.js
+++ msearch-app/src/main/resources/static/script.js
@@ -1,32 +1,42 @@
 $(document).ready(function() {
-	
-		var table = $("#jobTable");
-		var tBody = $("tBody");
-		
-		
-
-		$(document).on("input", "#search", function() {
-			var text = $('#search').val();
-			$.get("http://localhost:8080/search/from/job", {
-				text : text
-			}, function(response) {
-				if (response != null) {
-					$("#infoLbl").text('Total Jobs Found: '+response.length);
-					tBody.empty();
-					$.each(response, function(key, value) {
-						var row = $("<tr><td class=\"smallTd\"></td><td class=\"bigTd\"></td><td class=\"midTd\"></td></tr>");
-						row.children().eq(0).text(value['id']);
-						row.children().eq(1).text(value['title']);
-						row.children().eq(2).text(value['location']);
-						row.appendTo(tBody);
-					});//Loop ends
-				} else {
-					tBody.empty();
-					$("#infoLbl").text('');
-				}
-			});
+		$(document).on("click", "#searchBtn", function() {
+			getData();
 		});
 		
+		$('#search').keypress(function(event){
+			 var keycode = (event.keyCode ? event.keyCode : event.which);
+			    if(keycode == '13'){
+			        getData();
+			    }
+		});	
 		
-		
+});
+
+function getData(){
+	var table = $("#jobTable");
+	var tBody = $("tBody");
+	var text = $('#search').val();
+	$.get("http://localhost:8080/search/from/job", {
+		text : text
+	}, function(response) {
+		if (response != null) {
+			$("#infoLbl").text('Total Jobs Found: '+response.length);
+			tBody.empty();
+			$.each(response, function(key, value) {
+				var row = $("<tr><td class=\"smallTd\"></td><td class=\"bigTd\"></td><td class=\"midTd\"></td></tr>");
+				row.children().eq(0).text(value['id']);
+				row.children().eq(1).text(value['title']);
+				row.children().eq(2).text(value['location']);
+				row.appendTo(tBody);
+			});//Loop ends
+		} else {
+			tBody.empty();
+			$("#infoLbl").text('');
+		}
 	});
+   
+}
+
+
+
+
